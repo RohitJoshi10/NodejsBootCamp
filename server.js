c1b7e124ffdd3dc42f309ab23b2734a25ecbd547
Rohit <rohit11joshi10@gmail.com>
2024-03-29 00:08:51 +0530

    FS and OS Module Nodejs Done

---
+++ server.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+var os = require("os");
+
+var user = os.userInfo();
+console.log(user.username);
+
+fs.appendFile("greeting.txt", "Hi " + user.username + "!\n", () =>
+  console.log("file is created")
+);
+
+console.log(os);
+console.log(fs);
