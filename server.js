3b3f8a1c6fa9342a80b294ff41c56ff2a8eb1ad7
Rohit <rohit11joshi10@gmail.com>
2024-03-30 20:15:20 +0530

    Mongoose schema in node js | CRUD operation in node js with try catch statement

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
 const express = require("express");
 const app = express();
+const db = require("./db");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const Person = require("./models/Person");
+const res = require("express/lib/response");
 
 app.get("/", function (req, res) {
   res.send("Welcome to my Hotel...");
 });
 
-app.get("/chicken", function (req, res) {
-  res.send("Your chicken is going to be prepared soon...");
+// POST route to add a person
+app.post("/person", async (req, res) => {
+  try {
+    const data = req.body; // Assuming the request body contains the person data
+
+    // Create a new Person document using the Mongoose model.
+    const newPerson = new Person(data);
+
+    // Save the newPerson to the database
+    const response = await newPerson.save();
+    console.log("Data Saved");
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.get("/idli", (req, res) => {
-  var customized_idli = {
-    name: "rava idli",
-    size: "10 cm diameter",
-    is_sambhar: true,
-    is_chuteny: false,
-  };
-  res.send(customized_idli);
+app.get("/person", async (req, res) => {
+  try {
+    const data = await Person.find();
+    console.log("Data Fetched");
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(3000, () => {
